93e72bc312aac9591a13104209bb6d10aa5e7108
ElMarchk0 <petermc9011@gmail.com>
2024-02-05 20:31:21 -0800

    Fixed Spelling

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -1,43 +1,45 @@
-import { Badge, Box, Button, Image, Text } from '@chakra-ui/react'
-import { Product } from '../App'
+import { Badge, Box, Button, Image, Text } from "@chakra-ui/react";
+import { Product } from "../App";
 
 type Props = {
-  item: Product
-  addToCart: (item: Product) => void
-}
+  item: Product;
+  addToCart: (item: Product) => void;
+};
 
-const ProductCard: React.FC<Props> = ({item, addToCart}) => {
+const ProductCard: React.FC<Props> = ({ item, addToCart }) => {
   return (
-    <Box maxW='sm' borderWidth='1px' borderRadius='lg' roundedTop="lg" >
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" roundedTop="lg">
       <Image src={item.image} />
-      <Box p='6' backgroundColor='gray.50' >
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='teal'>
+      <Box p="6" backgroundColor="gray.50">
+        <Box display="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
             New
-          </Badge>          
+          </Badge>
         </Box>
 
-        <Box mt='1' fontWeight='semibold' as='h3' lineHeight='tight' isTruncated>
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h3"
+          lineHeight="tight"
+          isTruncated>
           {item.title}
         </Box>
         <Box>
-          <Text as='h3' lineHeight='tight' isTruncated >
+          <Text as="h3" lineHeight="tight" isTruncated>
             {item.category}
           </Text>
-          <Text lineHeight='tight' isTruncated > 
+          <Text lineHeight="tight" isTruncated>
             {item.description}
           </Text>
         </Box>
+        <Box>$ {item.price.toFixed(2)}</Box>
         <Box>
-          $ {item.price.toFixed(2)}
+          <Button onClick={() => addToCart(item)}>Add to Cart</Button>
         </Box>
-        <Box>
-          <Button onClick={() => addToCart(item)}>Add Cart</Button>
-        </Box>
-        
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
